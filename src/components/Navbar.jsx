6f2dee13a5b801b2128445ec1db7e9af4346b7cb
Hrishi <hrishikeshrs321@gmail.com>
2026-02-13 22:28:16 +0530

    Navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,28 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function Navbar({ user }) {
-  const pathName = usePathname();
-
-  const navLinks = [
-    {name: "Home", href: "/"},
-    {name: "Teachers", href: "/teachers"},
-    {name: "Parents", href: "/parents"},
-    {name: "Managment", href: "/management"},
-    {name: "Login", href: "/login"}
-  ];
-
   return (
-    <nav className="bg-gray-900 text-white px-6 py-4 shadow">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
+    <div className="fixed top-4 left-0 w-full flex justify-center z-50">
+      <nav className="bg-gray-200 text-gray-800 px-6 py-2 rounded-full shadow flex gap-6 text-sm font-medium">
 
-        <h1 className="text-xl font-bold">Sentinals</h1>
-
-        <div className="flex gap-6">
-          {navLinks.map((link) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className={`hover:text-blue-400 transition ${
-                pathName === link.href ? "text-blue-400 font-semibold" : ""
-              }`}
-            >
-              {link.name}
+        {user ? (
+          <>
+            <Link href="/dashboard" className="hover:text-black">
+              Dashboard
             </Link>
-          ))}
-        </div>
+            <Link href="/profile" className="hover:text-black">
+              Profile
+            </Link>
+          </>
+        ) : (
+          <Link href="/login" className="hover:text-black">
+            Login
+          </Link>
+        )}
 
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
